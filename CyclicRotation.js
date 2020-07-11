ee31954343426dda4803c1b7788c8a793d83809a
Idan Bercovici <idanb11@gmail.com>
2020-07-11 12:43:14 +0800

    add new challange

---
+++ CyclicRotation.js
@@ -0,0 +1,18 @@
+
+const assert = require('assert');
+
+function solution(A, K) {
+  let newArray = [...A];
+
+  for (let i = 0 ; i < K; i++) {
+    newArray.unshift(newArray[newArray.length - 1]);
+    newArray.pop();
+  }
+
+  return newArray;
+}
+
+
+assert.deepStrictEqual(solution([3, 8, 9, 7, 6], 3), [9, 7, 6, 3, 8]);
+assert.deepStrictEqual(solution([0, 0, 0], 1), [0, 0, 0]);
+assert.deepStrictEqual(solution([1, 2, 3, 4], 4), [1, 2, 3, 4]);
