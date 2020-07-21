f600deb8484c800bf8ff0640c601b30c377f67e0
Idan Bercovici <idanb11@gmail.com>
2020-07-21 15:08:58 +0800

    update code

--- mergeArrays.js
+++ mergeArrays.js
@@ -1,32 +1,23 @@
-//   const myArray = [3, 4, 6, 10, 11, 15];
-// const alicesArray = [1, 5, 8, 12, 14, 19];
-
-// console.log(mergeArrays(myArray, alicesArray));
-
-// logs [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
-
-
 const assert = require("assert").strict;
 
 function mergeArrays(arry1, arry2) {
+  let index1 = 0;
+  let index2 = 0;
   const result = [];
-  const maxLen = Math.max(arry1.length, arry2.length);
 
-  const minOrder = 0
-
-  for (let index = 0; index < maxLen; index++) {
-    const element1 = arry1[index];
-    const element2 = arry2[index];
-
-    
-
-    
+  while (arry1[index1] || arry2[index2]) {
+    if (arry1[index1] < arry2[index2]) {
+      result.push(arry1[index1]);
+      index1++;
+    } else {
+      result.push(arry2[index2]);
+      index2++;
+    }
   }
-
-  // const set = new Set(arry1.concat(arry2));
-  // return Array.from(set).sort();
-
-
+  return result;
 }
 
-assert.strictEqual(mergeArrays([3, 4, 6, 10, 11, 15], [1, 5, 8, 12, 14, 19]), [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]);
+assert.deepStrictEqual(
+  mergeArrays([3, 4, 6, 10, 11, 15], [1, 5, 8, 12, 14, 19]),
+  [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
+);
