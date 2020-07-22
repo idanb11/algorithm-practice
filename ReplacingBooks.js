bd3cd0d133beb087baa339b9377a488a7388e26b
Idan Bercovici <idanb11@gmail.com>
2020-07-22 10:51:52 +0800

    updating

--- ReplacingBooks.js
+++ ReplacingBooks.js
@@ -9,12 +9,12 @@ function solution(A, K) {
   for (; pointer1 < A.length; ) {
     if (A[pointer1] === A[pointer2]) {
       pointer2++;
-    } else if (replaceableChars > 0) {
+    } else if (replaceableChars > 0 && pointer2 < A.length) {
       pointer2++;
       replaceableChars--;
     } else {
       maxResult = Math.max(maxResult, pointer2 - pointer1);
-      if (pointer2 === A.length) {
+      if (pointer2 >= A.length) {
         break;
       }
       pointer1++;
@@ -25,8 +25,7 @@ function solution(A, K) {
   return maxResult;
 }
 
-assert.strictEqual(solution([4, 5, 5, 4, 2, 2, 4], 0), 2);
-
-assert.strictEqual(solution([1, 1, 3, 4, 3, 3, 4], 2), 5);
-
-assert.strictEqual(solution([1, 3, 3, 2], 2), 4);
+// assert.strictEqual(solution([1, 3, 3, 2], 2), 4);
+// assert.strictEqual(solution([4, 5, 5, 4, 2, 2, 4], 0), 2);
+// assert.strictEqual(solution([1, 1, 3, 4, 3, 3, 4], 2), 5);
+assert.strictEqual(solution([3, 3, 3], 1), 3);
